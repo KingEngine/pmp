3e7335d11b15f38b16babb22a109593ab733049c
king <790467095@qq.com>
2015-02-11 16:06:36 +0800

    提交

--- pmp/pmp-web/src/main/java/com/chinaebi/pmp/web/controller/LoginController.java
+++ pmp/pmp-web/src/main/java/com/chinaebi/pmp/web/controller/LoginController.java
@@ -1,15 +1,28 @@
 package com.chinaebi.pmp.web.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.support.SessionStatus;
+
+import com.chinaebi.pmp.common.constant.Annotations;
+import com.chinaebi.pmp.common.constant.WebConstants;
+import com.chinaebi.pmp.common.exception.BusinessException;
+import com.chinaebi.pmp.database.entity.Menu;
+import com.chinaebi.pmp.service.ILoginService;
 /**
  * 登录控制器
  * @author king 
@@ -17,7 +30,11 @@ import org.springframework.web.bind.support.SessionStatus;
  */
 @Controller
 public class LoginController {
+	private Logger logger = LoggerFactory.getLogger(getClass());
 	public final static String prefix="";
+	@Autowired
+	@Qualifier(Annotations.SERVICE_LOGIN)
+	private ILoginService loginServie;
 	/**
 	 * 登陆操作
 	 * @param session
@@ -36,7 +53,15 @@ public class LoginController {
 	 * @return
 	 */
 	@RequestMapping(value="/getMainFrame.do",method={RequestMethod.GET,RequestMethod.POST})
-	public String getMainFrame(){
+	public String getMainFrame(HttpSession session){
+		//查找用户的菜单权限
+		String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+		try {
+			List<Menu> firstMenus = loginServie.getUsersFirstMenuList(userName);
+			session.setAttribute(WebConstants.FIRST_MENUS, firstMenus);;
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
 		return prefix+"main";
 	}
 	/**
